21244689d5a98447a8e61d9f2184abc3b3bcf157
CallumHoughton18 <callum.houghton13@hotmail.co.uk>
2022-06-15 18:00:23 +0100

    WIP: add procedure and statement parsing

--- JackCompiler.Core/Parser/JackParser.cs
+++ JackCompiler.Core/Parser/JackParser.cs
@@ -14,50 +14,64 @@ sealed class JackParser
  
     public ParserTree ParseTokens()
     {
+        var grammarStack = new Stack<BaseGrammar>();
+        BaseGrammar currentGrammar = null;
         var parserTree = new ParserTree();
-
-
-
-        foreach (var token in _tokens)
+        
+        for (int i = 0; i < _tokens.Count;)
         {
-            if (token.TokenValue == "class" && parserTree.CurrentNode?.Name != "class")
+            var token = _tokens[i];
+            if (token.TokenValue == "class")
             {
-                parserTree.AddNoneTerminalNodeAndAdvance(new NoneTerminalNode(parserTree.CurrentNode, "class"));
+                var newClass = new ClassGrammar();
+                grammarStack.Push(newClass);
+                currentGrammar = grammarStack.Peek();
+                parserTree.AddNoneTerminalNodeAndAdvance(new NoneTerminalNode(parserTree.CurrentNode, currentGrammar.Name));
             }
-            if (parserTree.CurrentNode?.Name == "class" && token.TokenValue == "}")
-            {
-                parserTree.SetCurrentNodeToPrevious();
-            }
-            
-            if (token.TokenValue == "function" && parserTree.CurrentNode?.Name != "subRoutineDec")
-            {
-                parserTree.AddNoneTerminalNodeAndAdvance(new NoneTerminalNode(parserTree.CurrentNode, "subRoutineDec"));
-            }
-            if (parserTree.CurrentNode?.Name == "subRoutineDec" && token.TokenValue == "}")
-            {
-                parserTree.SetCurrentNodeToPrevious();
-            }
-            
-            //TODO: need to add these terminal ones to the correct current section, which is being reset above
-            parserTree.AddTerminalNode(new TerminalNode(parserTree.CurrentNode, token));
 
+            var (isNewGrammar, newGrammar, addTokenToNewGrammar) = currentGrammar.IsNewChild(token);
+            if (isNewGrammar)
+            {
+                grammarStack.Push(newGrammar);
+                var prevGrammar = currentGrammar as StatementsGrammar;
+                var prev1Grammar = currentGrammar as WhileStatementGrammar;
+                // If prev grammar is a statement, and new grammar is a statements, then i should also advance.
+                
+                currentGrammar = newGrammar;
+                if (!addTokenToNewGrammar.GetValueOrDefault(true)) parserTree.AddTerminalNode(new TerminalNode(parserTree.CurrentNode, token));
+                parserTree.AddNoneTerminalNodeAndAdvance(new NoneTerminalNode(parserTree.CurrentNode, currentGrammar.Name));
+                if (prevGrammar is null && currentGrammar.ShouldAdvanceToNextToken) i++;
+                if (prev1Grammar is not null && (currentGrammar as StatementsGrammar) != null) i++;
+                continue;
+            }
+
+            var (endOfGrammar, addTokenToCurrentGrammar) = currentGrammar.IsEndOfGrammar(token);
+            if (endOfGrammar)
+            {
+
+                if (addTokenToCurrentGrammar) parserTree.AddTerminalNode(new TerminalNode(parserTree.CurrentNode, token));
+                if (grammarStack.Count > 0)
+                {
+                    grammarStack.Pop();
+                    if (grammarStack.Count > 0)
+                    {
+                        currentGrammar = grammarStack.Peek();
+
+                    }
+                }
+                parserTree.SetCurrentNodeToPrevious();
+                
+                string[] statementBeginnings = { "let", "if", "while", "do", "return" };
+                var forceAdvance = i < _tokens.Count - 2 && statementBeginnings.Any(x => x ==_tokens[i+1].TokenValue);
+                
+                if (currentGrammar.ShouldAdvanceToNextToken || forceAdvance) i++;
+                if (!addTokenToCurrentGrammar) parserTree.AddTerminalNode(new TerminalNode(parserTree.CurrentNode, token));
+                continue;
+            }
             
+            parserTree.AddTerminalNode(new TerminalNode(parserTree.CurrentNode, token));
+            if (currentGrammar.ShouldAdvanceToNextToken) i++;
         }
         return parserTree;
     }
 }
-
-sealed class JackGrammarSection
-{
-    public JackGrammarSection(string grammarName)
-    {
-        GrammarName = grammarName;
-        Tokens = new List<Token>();
-        OtherGrammars = new List<JackGrammarSection>();
-    }
-    
-    public string GrammarName { get; }
-    public IList<Token> Tokens { get;}
-    public IList<JackGrammarSection> OtherGrammars { get; }
-}
-
